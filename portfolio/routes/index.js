f7dbea06e93f699f029244c66052b53609d64901
Rupinder Singh <is.rupinder23@gmail.com>
2022-10-03 10:11:13 -0400

    Site structure completed

--- portfolio/routes/index.js
+++ portfolio/routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', 
+  
+  { title: 'Express' ,
+  h1: 'Main Heading'
+
+
+});
 });
 
 module.exports = router;
