a3e99c06d0d967f233e143ab49ffa09603f1d592
Rupinder Singh <is.rupinder23@gmail.com>
2022-10-03 10:48:35 -0400

    Routing done

--- portfolio/routes/index.js
+++ portfolio/routes/index.js
@@ -3,13 +3,37 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Home'});
+});
+
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Home'});
+});
+
+/* GET About us page. */
+router.get('/about', function(req, res, next) {
   res.render('index', 
   
-  { title: 'Express' ,
-  h1: 'Main Heading'
-
-
+  { title: 'About'});
 });
+/* GET Products page. */
+router.get('/products', function(req, res, next) {
+  res.render('index', 
+  
+  { title: 'Products'});
+});
+/* GET services page. */
+router.get('/services', function(req, res, next) {
+  res.render('index', 
+  
+  { title: 'Services'});
 });
 
+/* GET contact us page. */
+router.get('/contact', function(req, res, next) {
+  res.render('index', 
+  
+  { title: 'Contact'});
+});
 module.exports = router;
